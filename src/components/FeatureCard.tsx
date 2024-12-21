690abaed2c4bb9a54d3dbfa014fea1dd5060ef1a
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-12-21 18:25:33 +0530

    text alignment fixed

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -8,7 +8,7 @@ function FeatureCard({ title, description, icon }: FeatureCardProps) {
       <div className="h-20 w-20 lg:h-28 lg:w-28 bg-white text-secondary rounded-full flex justify-center items-center">
         <Image  src={icon} alt={title} className="h-14 w-14 lg:h-20 lg:w-20" />
       </div>
-      <h4 className="text-xl lg:text-2xl font-bold">{title}</h4>
+      <h4 className="text-xl lg:text-2xl font-bold text-center">{title}</h4>
       <p className="leading-5 text-xs md:text-sm text-center max-w-[250px]">
         {description}
       </p>
